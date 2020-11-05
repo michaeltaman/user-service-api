6cbd068e3a1b10b6048a895bf0cb2e2c27050c8a
michaeltaman <mictrwork@gmail.com>
2020-11-05 12:12:54 +0200

    Added react etc.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3001;
 
 app.get('/', (req, res) => {
-  res.json([{
-      name: 'Michael',
-      email: 'mictrwork@gmail.com'
-  }])
-})
+  res.json([
+    {
+      name: 'Bob',
+      email: 'bob@gmail.com',
+    },
+    {
+      name: 'Alice',
+      email: 'alice@hotmail.com',
+    }
+  ]);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
